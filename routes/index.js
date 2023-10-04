3afc787754ef57ecce91a9833de403115d34106c
Lindsey Howard <codelikeagirl91@gmail.com>
2023-10-04 16:55:30 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
-const express = require("express");
+// Require Express, declare router and require project JSON data
+const express = require('express');
+const {projects} = require('../data.json');
+
 const router = express.Router();
-const data = require("../data.json");
-const { projects } = require("../data.json");
 
-// root route
-router.get("/", (req, res) => {
-	res.render("index", { projects });
+// Create routes for main and about pages
+// On main page, pass projects data into route via locals variable
+router.get('/', (req, res) => {
+    res.locals.projects = projects;
+    res.render('index');
 });
 
-// route for about page
-router.get("/about", (req, res) => {
-	res.render("about");
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
+// Export Router
 module.exports = router;
