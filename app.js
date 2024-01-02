96d120d5561cf7f0d4c5c5ebf1a8ad2ae69c5664
Lindsey Howard <codelikeagirl91@gmail.com>
2024-01-02 09:35:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 // Configure Express and static files
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Declare and configure routes
